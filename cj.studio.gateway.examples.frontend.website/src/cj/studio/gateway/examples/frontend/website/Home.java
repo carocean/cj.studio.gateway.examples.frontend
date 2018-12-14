a0a13ab26237840ab8886395a57c1f9de23d6d40
caroceanjofers <carocean.jofers@icloud.com>
2018-12-14 17:05:48 +0800

    frontends-1.0.20181214.1705

--- cj.studio.gateway.examples.frontend.website/src/cj/studio/gateway/examples/frontend/website/Home.java
+++ cj.studio.gateway.examples.frontend.website/src/cj/studio/gateway/examples/frontend/website/Home.java
@@ -6,6 +6,9 @@ import cj.studio.ecm.annotation.CjServiceRef;
 import cj.studio.ecm.net.Circuit;
 import cj.studio.ecm.net.CircuitException;
 import cj.studio.ecm.net.Frame;
+import cj.studio.ecm.net.io.MemoryContentReciever;
+import cj.studio.ecm.net.io.MemoryInputChannel;
+import cj.studio.ecm.net.io.MemoryOutputChannel;
 import cj.studio.gateway.socket.app.IGatewayAppSiteResource;
 import cj.studio.gateway.socket.app.IGatewayAppSiteWayWebView;
 import cj.studio.gateway.socket.pipeline.IOutputSelector;
@@ -18,22 +21,27 @@ public class Home implements IGatewayAppSiteWayWebView {
 
 	@Override
 	public void flow(Frame f, Circuit c, IGatewayAppSiteResource ctx) throws CircuitException {
-//		Element doc=ctx.html("/index.html");
-//		test1(f,c);
-		test2(f, c);
-//		c.content().writeBytes(doc.html().getBytes());
-	}
+		IOutputer back = selector.select("backend");// 回发
 
-	void test2(Frame f, Circuit c) throws CircuitException {
-		Frame req = new Frame("get /uc/ http/1.1");
-		Circuit feeds = new Circuit("http/1.1 200 ok");
-		IOutputer output = selector.select("uc");
-		output.send(req, feeds);
-		c.fillFrom(feeds);
-//		output.closePipeline();
-		output.releasePipeline();
-	}
+		MemoryInputChannel in = new MemoryInputChannel(8192);
+		Frame f1 = new Frame(in, "post /uc/ http/1.1");
+		f1.contentType("application/x-www-form-urlencoded");
 
-	
+		MemoryContentReciever mcr = new MemoryContentReciever();
+		f1.content().accept(mcr);
+		in.begin(null);
+		byte[] b = "name=zhaoxb&type=1&age=10&dept=国务院".getBytes();
+		in.done(b, 0, b.length);
+
+		MemoryOutputChannel out = new MemoryOutputChannel();
+		Circuit c1 = new Circuit(out, "http/1.1 200 ok");
+
+		back.send(f1, c1);
+
+		byte[] ret = out.readFully();
+		c.content().writeBytes(ret);
+
+		back.closePipeline();
+	}
 
 }
