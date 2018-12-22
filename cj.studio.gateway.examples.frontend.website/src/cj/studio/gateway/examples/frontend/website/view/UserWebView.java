3fa421a53f5130793b9bd93a81628e3222e61ad3
caroceanjofers <carocean.jofers@icloud.com>
2018-12-22 17:46:05 +0800

    frontends-1.0.20181222.1746

--- cj.studio.gateway.examples.frontend.website/src/cj/studio/gateway/examples/frontend/website/view/UserWebView.java
+++ cj.studio.gateway.examples.frontend.website/src/cj/studio/gateway/examples/frontend/website/view/UserWebView.java
@@ -1,5 +1,7 @@
 package cj.studio.gateway.examples.frontend.website.view;
 
+import java.util.List;
+
 import cj.studio.ecm.annotation.CjBridge;
 import cj.studio.ecm.annotation.CjService;
 import cj.studio.ecm.net.Circuit;
@@ -11,7 +13,7 @@ import cj.studio.gateway.socket.app.IGatewayAppSiteResource;
 import cj.studio.gateway.socket.app.IGatewayAppSiteWayWebView;
 import cj.studio.gateway.stub.annotation.CjStubRef;
 
-@CjBridge(aspects = "rest")
+@CjBridge(aspects = "@rest")
 @CjService(name = "/user.html")
 public class UserWebView implements IGatewayAppSiteWayWebView {
 
@@ -20,7 +22,17 @@ public class UserWebView implements IGatewayAppSiteWayWebView {
 	@Override
 	public void flow(Frame f, Circuit c, IGatewayAppSiteResource ctx) throws CircuitException {
 		UserBO bo=user.getUser("1001479428");
-		System.out.println("------"+bo);
+		System.out.println("----1--"+bo);
+		UserBO bo1=new UserBO();
+		bo1.setAge(33);
+		String id=f.hashCode()+"";
+		bo1.setId(id);
+		bo1.setName("wuj");
+		user.save(bo1);
+		UserBO bo2=user.getUser(id);
+		System.out.println("----2--"+bo2);
+		List<UserBO> list=user.query("wuj", 33);
+		System.out.println("----3--"+list.size());
 	}
 
 }
