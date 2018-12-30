b4c272dcb5e5461adf05349e82ac29ba64c0b3a9
caroceanjofers <carocean.jofers@icloud.com>
2018-12-30 11:29:46 +0800

    frontends-1.0.20181230.1129

---
+++ cj.studio.gateway.examples.frontend.website/src/cj/studio/gateway/examples/frontend/website/view/DeptWebView.java
@@ -0,0 +1,35 @@
+package cj.studio.gateway.examples.frontend.website.view;
+
+import java.util.Date;
+import java.util.List;
+
+import cj.studio.ecm.annotation.CjBridge;
+import cj.studio.ecm.annotation.CjService;
+import cj.studio.ecm.net.Circuit;
+import cj.studio.ecm.net.CircuitException;
+import cj.studio.ecm.net.Frame;
+import cj.studio.gateway.examples.backend.usercenter.bo.DeptmentBO;
+import cj.studio.gateway.examples.backend.usercenter.stub.IDeptStub;
+import cj.studio.gateway.socket.app.IGatewayAppSiteResource;
+import cj.studio.gateway.socket.app.IGatewayAppSiteWayWebView;
+import cj.studio.gateway.stub.annotation.CjStubRef;
+
+@CjBridge(aspects = "@rest")
+@CjService(name = "/dept.html")
+public class DeptWebView implements IGatewayAppSiteWayWebView {
+
+	@CjStubRef(remote = "rest://backend/uc/", stub = IDeptStub.class)
+	IDeptStub dept;
+	@Override
+	public void flow(Frame f, Circuit c, IGatewayAppSiteResource ctx) throws CircuitException {
+		DeptmentBO bo=new DeptmentBO();
+		bo.setCreateDate(new Date());
+		bo.setLeader("cj");
+		bo.setName("wangk");
+		dept.save(bo);
+		
+		List<DeptmentBO> list=dept.getAll();
+		System.out.println(list.size());
+	}
+
+}
