dbe4e9108be48e2ade2e92a606266c6dd6afcae8
caroceanjofers <caroceanjofers@192.168.41.104>
2019-04-02 23:41:45 +0800

    frontend-1.0.20190402.2341

--- cmdtools/gateway/gateway.sh
+++ cmdtools/gateway/gateway.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #shell call java program
 echo $JAVA_HOME
-java -Xms50m -Xmx850m -XX:MaxDirectMemorySize=512m -jar  gateway-1.0.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -Xms50m -Xmx850m -XX:MaxDirectMemorySize=512m -jar  gateway-1.0.0.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
 exit 0;
